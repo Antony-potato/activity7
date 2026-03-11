847e3372856432bf855c02dcfffba5acf8f79679
Antony-potato <Al02956632@tecmilenio.mx>
2026-03-11 00:12:58 -0500

    Se agregan seeders de usuarios y kits, factory de 100 cursos y README actualizado

---
+++ database/seeders/RoboticsKitSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoboticsKitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
